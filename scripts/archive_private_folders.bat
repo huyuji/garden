0f1524e89390693dfcc2f9cb26966c6bbb47c6f8
Yuji <yuji.hu@curvedental.com>
2018-05-28 11:11:34 -0600

    regular addition

---
+++ scripts/archive_private_folders.bat
@@ -0,0 +1,13 @@
+@echo off
+set dir=%1%
+set password=%2%
+
+pushd %dir%
+for /D %%d in (*) do (
+    echo "%%d"
+    7z a -p%password% "%%d.zip" "%%d" || GOTO :end
+    rd /s /q "%%d"
+)
+
+:end
+popd
