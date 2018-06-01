6aff72f008ad7092c7649ca671da8e0d01b17c57
Yuji <yuji.hu@curvedental.com>
2018-06-01 08:32:41 -0600

    regular

---
+++ scripts/utility/work.bat
@@ -0,0 +1,18 @@
+rem common
+start "" "C:\Program Files\Google\Drive\googledrivesync.exe"
+start "" "C:\Program Files\Everything\Everything.exe"
+start "" "e:\program\Ditto\ditto.exe"
+start "" "E:\program\AutoHotkey\AutoHotkey.exe" "E:\code\garden\scripts\wordexpander.ahk"
+start "" "C:\Program Files (x86)\Microsoft Office\root\Office16\ONENOTE.EXE"
+start "" "e:\program\freecommander\FreeCommander.exe"
+start "" "C:\Program Files (x86)\WizNote\wiz.exe"
+start "" "C:\Users\Yuji\AppData\Roaming\baidu\BaiduNetdisk\BaiduNetdisk.exe"
+
+rem work
+start "" "C:\Program Files (x86)\Atlassian\HipChat4\HipChat.exe"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 1"
+start "" "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" --comment "curvedev-encrypted" --startvm "13a8645e-73b4-4b3d-aaab-73a28676d20f"
+
+rem home
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Default"
+start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
