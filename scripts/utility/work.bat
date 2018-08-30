ebebb93e0a62f74b4c5d86cfa331ef78586a409f
Yuji <yuji.hu@curvedental.com>
2018-08-30 10:31:04 -0600

    regular

--- scripts/utility/work.bat
+++ scripts/utility/work.bat
@@ -11,7 +11,7 @@ rem start "" "C:\Users\Yuji\AppData\Roaming\baidu\BaiduNetdisk\BaiduNetdisk.exe"
 rem work
 start "" "C:\Program Files (x86)\Atlassian\HipChat4\HipChat.exe"
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 1"
-start "" "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" --comment "curvedev-encrypted" --startvm "13a8645e-73b4-4b3d-aaab-73a28676d20f"
+rem start "" "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" --comment "curvedev-encrypted" --startvm "13a8645e-73b4-4b3d-aaab-73a28676d20f"
 
 rem home
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Default"
