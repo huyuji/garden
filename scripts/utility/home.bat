9f75172e63012d808045a8fce89a6884cea53167
Yuji Hu <huyuji@users.noreply.github.com>
2019-01-25 22:29:59 -0700

    regular

--- scripts/utility/home.bat
+++ scripts/utility/home.bat
@@ -1,13 +1,13 @@
 rem common
 start "" "C:\Program Files\Google\Drive\googledrivesync.exe"
-rem start "" "C:\Program Files\Everything\Everything.exe"
-start "" "e:\program\Ditto\ditto.exe"
-start "" "E:\program\AutoHotkey\AutoHotkey.exe" "E:\code\garden\scripts\wordexpander.ahk"
+start "" "C:\Program Files\Everything\Everything.exe"
+start "" "C:\Program Files\Ditto\ditto.exe"
+start "" "C:\Program Files\AutoHotkey\AutoHotkey.exe" "D:\Documents\GitHub\garden\scripts\wordexpander.ahk"
 rem start "" "C:\Program Files (x86)\Microsoft Office\root\Office16\ONENOTE.EXE"
-start "" "e:\program\freecommander\FreeCommander.exe"
+start "" "C:\Program Files (x86)\FreeCommanderXE\FreeCommander.exe"
 rem start "" "C:\Program Files (x86)\WizNote\wiz.exe"
 rem start "" "C:\Users\Yuji\AppData\Roaming\baidu\BaiduNetdisk\BaiduNetdisk.exe"
 
 rem home
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Default"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 1"
 start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
