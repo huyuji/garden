8d6b6aa15c075181e4d8d3ad685e19f694a14310
Yuji Hu <huyuji@users.noreply.github.com>
2019-02-01 09:08:08 -0700

    regular

--- scripts/utility/home.bat
+++ scripts/utility/home.bat
@@ -7,6 +7,7 @@ rem start "" "C:\Program Files (x86)\Microsoft Office\root\Office16\ONENOTE.EXE"
 start "" "C:\Program Files (x86)\FreeCommanderXE\FreeCommander.exe"
 rem start "" "C:\Program Files (x86)\WizNote\wiz.exe"
 rem start "" "C:\Users\Yuji\AppData\Roaming\baidu\BaiduNetdisk\BaiduNetdisk.exe"
+start "" "C:\Program Files (x86)\eudic\eudic.exe"
 
 rem home
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 1"
