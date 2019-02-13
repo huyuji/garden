05f1b4453615c1765f84e66c3a3a07a1d0799c0a
Yuji <yuji.hu@curvedental.com>
2019-02-13 15:26:54 -0700

    regular

--- scripts/utility/work.bat
+++ scripts/utility/work.bat
@@ -1,12 +1,13 @@
 rem common
 rem start "" "C:\Program Files\Google\Drive\googledrivesync.exe"
-start "" "C:\Program Files\Everything\Everything.exe"
+rem start "" "C:\Program Files\Everything\Everything.exe"
 start "" "C:\Program Files\Ditto\ditto.exe"
 start "" "C:\Program Files\AutoHotkey\AutoHotkey.exe" "E:\code\garden\scripts\wordexpander.ahk"
 rem start "" "C:\Program Files (x86)\Microsoft Office\root\Office16\ONENOTE.EXE"
-start "" "C:\Program Files (x86)\FreeCommanderXE\FreeCommander.exe"
+start "" "C:\Program Files (x86)\FreeCommander XE\FreeCommander.exe"
 rem start "" "C:\Users\Yuji\AppData\Roaming\baidu\BaiduNetdisk\BaiduNetdisk.exe"
 start "" "C:\Program Files (x86)\eudic\eudic.exe"
+start "" "C:\Users\Yuji\AppData\Local\Glip\Update.exe --processStart RingCentral.exe"
 
 rem work
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 1"
