408ed92a444e724f51414b8e122e83c5833d325f
Yuji Hu <huyuji@users.noreply.github.com>
2019-02-15 21:13:34 -0700

    regular

--- scripts/utility/work.bat
+++ scripts/utility/work.bat
@@ -1,6 +1,6 @@
 rem common
 rem start "" "C:\Program Files\Google\Drive\googledrivesync.exe"
-start "" "C:\Program Files\Everything\Everything.exe"
+rem start "" "C:\Program Files\Everything\Everything.exe"
 start "" "C:\Program Files\Ditto\ditto.exe"
 start "" "C:\Program Files\AutoHotkey\AutoHotkey.exe" "E:\code\garden\scripts\wordexpander.ahk"
 start "" "C:\Program Files (x86)\Microsoft Office\root\Office16\ONENOTE.EXE"
@@ -9,11 +9,11 @@ rem start "" "C:\Users\Yuji\AppData\Roaming\baidu\BaiduNetdisk\BaiduNetdisk.exe"
 start "" "C:\Program Files (x86)\eudic\eudic.exe"
 
 rem work
-start "" "C:\Program Files (x86)\Atlassian\HipChat4\HipChat.exe"
+rem start "" "C:\Program Files (x86)\Atlassian\HipChat4\HipChat.exe"
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 1"
 rem start "" "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" --comment "curvedev-encrypted" --startvm "13a8645e-73b4-4b3d-aaab-73a28676d20f"
 rem start "" "C:\Program Files (x86)\Hourglass\Hourglass.exe"
 
 rem home
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Default"
+rem start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Default"
 start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
