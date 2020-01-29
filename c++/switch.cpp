470d35dfda90c838c12f3d0177551697a257ecb9
Yuji Hu <yuji.hu@curvedental.com>
2020-01-29 09:54:43 -0700

    regular

---
+++ c++/switch.cpp
@@ -0,0 +1,21 @@
+int main()
+{
+	int a = 0;
+	switch(a) {
+	case 1:
+		break;
+	case 1:
+		break;
+	default:
+		break;
+	}
+}
+
+/*
+switch.cpp:7:7: error: duplicate case value '1'
+        case 1:
+             ^
+switch.cpp:5:7: note: previous case defined here
+        case 1:
+             ^
+*/
