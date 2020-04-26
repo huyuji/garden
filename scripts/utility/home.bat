506d13319b46638e60ab60ebba58f845e022c42b
Yuji Hu <yuji.hu@curvedental.com>
2020-04-26 09:37:01 -0600

    regular

---
+++ scripts/utility/home.bat
@@ -0,0 +1,12 @@
+start "" "C:\Program Files (x86)\FreeCommander XE\FreeCommander.exe"
+start "" "C:\Program Files (x86)\Hourglass\Hourglass.exe"
+rem start "" D:\portables\Insomnia.exe
+rem start "" D:\private\code\garden\scripts\autohotkey\hotstring.ahk
+rem start "" D:\private\code\garden\scripts\autohotkey\jira.ahk
+start "" "C:\Program Files (x86)\WeChat\WeChat.exe"
+rem start "" C:\Users\Yuji\AppData\Local\Glip\RingCentral.exe
+rem start "" "C:\Program Files\OpenVPN Connect\OpenVPNConnect.exe"
+start "" "C:\Program Files (x86)\eudic\eudic.exe"
+start "" "C:\Program Files (x86)\Microsoft\BingDesktop\BingDesktop.exe"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start "" C:\Users\Yuji\AppData\Local\Todoist\WindowsDesktopApp\Todoist.exe
