eb701263226f3440393c217e6fe9aa378c195022
Yuji Hu <yuji.hu@curvedental.com>
2020-05-13 10:16:24 -0600

    regular

--- scripts/utility/backup-google-chrome-bookmarks.bat
+++ scripts/utility/backup-google-chrome-bookmarks.bat
@@ -1,1 +1,1 @@
-for /f usebackq %%F in (`date /T`) do copy "C:\Users\Yuji\AppData\Local\Google\Chrome\User Data\Profile 1\Bookmarks" D:\private\googledrives\googledrive-huyuji1986\backup\bookmarks\%%F
+for /f usebackq %%F in (`date /T`) do copy "C:\Users\Yuji\AppData\Local\Google\Chrome\User Data\Profile 1\Bookmarks" D:\private\googledrive-huyuji1986\backup\bookmarks\%%F
--- scripts/utility/work.bat
+++ scripts/utility/work.bat
@@ -10,3 +10,5 @@ start "" "C:\Program Files (x86)\eudic\eudic.exe"
 start "" "C:\Program Files (x86)\Microsoft\BingDesktop\BingDesktop.exe"
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 start "" C:\Users\Yuji\AppData\Local\Todoist\WindowsDesktopApp\Todoist.exe
+start "" "C:\Program Files\Microsoft Office\root\Office16\ONENOTE.EXE"
+start "" "C:\Program Files\VirtualBox\VirtualBoxVM.exe" --comment "devvm" --startvm "{3744bffa-2c4e-4093-a013-543e18f874e0}"
