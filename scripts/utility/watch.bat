8bcd995ae85fcf0040466cb03d5b8a6fccfd8f27
Yuji Hu <yuji.hu@curvedental.com>
2020-07-17 09:09:11 -0600

    regular

--- scripts/utility/watch.bat
+++ scripts/utility/watch.bat
@@ -1,20 +1,20 @@
+rem crucible
+start https://crucible.curvedms.org/cru?filter=inbox
+
+rem work email
+start https://mail.google.com/mail/ca/u/1/#inbox
+
+rem email
+start https://mail.google.com/mail/ca/u/0/#inbox
+
 rem comost giveaway
 start https://www.calgary.ca/UEP/WRS/Pages/Recycling-information/Residential-services/Green-cart/Green-Cart-Compost-Giveaway.aspx
 
 rem paw patrol helmet
 start https://www.toysrus.ca/en/Paw-Patrol-Toddler-Skye-Helmet/C48F0AAD.html
 
-rem work email
-start https://mail.google.com/mail/ca/u/1/#inbox
-
-rem crucible
-start https://crucible.curvedms.org/cru?filter=inbox
-
 rem work calendar
 start https://calendar.google.com/calendar/b/1/r/week?pli=1
-
-rem email
-start https://mail.google.com/mail/ca/u/0/#inbox
 
 rem calendar
 start https://calendar.google.com/calendar/r/week
