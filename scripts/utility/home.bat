ac3b4d76f85bccfbb80de2d0ddf7c7f39b60489c
Yuji Hu <huyuji@users.noreply.github.com>
2020-10-11 10:39:26 -0600

    regular

--- scripts/utility/home.bat
+++ scripts/utility/home.bat
@@ -1,13 +1,12 @@
+start "" "C:\Users\Gardener\AppData\Local\Wox\Wox.exe"
 start "" "C:\Program Files (x86)\FreeCommander XE\FreeCommander.exe"
 start "" "C:\Program Files (x86)\Hourglass\Hourglass.exe"
 rem start "" D:\portables\Insomnia.exe
 rem start "" D:\private\code\garden\scripts\autohotkey\hotstring.ahk
 rem start "" D:\private\code\garden\scripts\autohotkey\jira.ahk
 start "" "C:\Program Files (x86)\WeChat\WeChat.exe"
-rem start "" C:\Users\Yuji\AppData\Local\Glip\RingCentral.exe
-rem start "" "C:\Program Files\OpenVPN Connect\OpenVPNConnect.exe"
 start "" "C:\Program Files (x86)\eudic\eudic.exe"
-rem start "" "C:\Program Files (x86)\Microsoft\BingDesktop\BingDesktop.exe"
+start "" "C:\Program Files (x86)\Microsoft\BingDesktop\BingDesktop.exe"
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 start "" C:\Users\Yuji\AppData\Local\Todoist\WindowsDesktopApp\Todoist.exe
-start "" "C:\Program Files\Microsoft Office\root\Office16\ONENOTE.EXE"
+rem start "" "C:\Program Files\Microsoft Office\root\Office16\ONENOTE.EXE"
