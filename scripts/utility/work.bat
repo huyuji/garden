6d5913e8b3f78e69138707eb0a11b645c994b1c3
huyuji <huyuji@users.noreply.github.com>
2020-10-30 08:25:15 -0600

    Update work.bat

--- scripts/utility/work.bat
+++ scripts/utility/work.bat
@@ -5,7 +5,7 @@ start "" "C:\Program Files\OpenVPN\bin\openvpn-gui.exe"
 start "" "C:\Users\yuji.hu\AppData\Local\Wox\app-1.4.1196\wox.exe"
 start "" "C:\Program Files (x86)\Hourglass\hourglass.exe"
 start "" "C:\Program Files (x86)\eudic\eudic.exe"
-start "" "C:\Users\yuji.hu\Documents\private\shortcuts.ahk"
+start "" "C:\portables\shortcuts.ahk"
 start "" "C:\portables\cmder_mini\cmder.exe"
 start "" https://clockify.me/timesheet
 start "" "C:\Users\yuji.hu\AppData\Local\Microsoft\Teams\current\Teams.exe"
